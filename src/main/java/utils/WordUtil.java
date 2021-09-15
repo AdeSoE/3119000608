9c654124238befbf052cc1ab9d94055485a2475a
AdeSoE <shh0313@hotmail.com>
2021-09-15 13:48:42 +0800

    第2次提交，添加计算重复率算法、添加剔除非关键字功能。

--- src/main/java/utils/WordUtil.java
+++ src/main/java/utils/WordUtil.java
@@ -4,11 +4,14 @@ import com.hankcs.hanlp.corpus.document.sentence.word.Word;
 import com.hankcs.hanlp.seg.common.Term;
 import com.hankcs.hanlp.tokenizer.StandardTokenizer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WordUtil {
 
+    //字符串分词为List
     public static List<Word> txtStr2WordList(String text) {
         //HanLP标准分词得到Term的List
         List<Term> termList = StandardTokenizer.segment(text);
@@ -16,7 +19,23 @@ public class WordUtil {
         List<Word> wordList = termList.stream().map(term ->
                 new Word(term.word, term.nature.toString())).collect(Collectors.toList());
 
+        eliminateNonKeyword(wordList);
+
         return wordList;
+    }
+
+    //剔除非关键字
+    public static void eliminateNonKeyword(List<Word> wordList) {
+        wordList.removeIf(word -> word.getLabel().charAt(0) == 'w');
+    }
+
+    public static Map<String, Integer> getWordFreq(List<Word> words) {
+        Map<String, Integer> freq = new HashMap<>();
+        //words.forEach(i -> freq.computeIfAbsent(i.getLabel(), k -> 1);
+        words.forEach(word ->
+            freq.put(word.getValue(), freq.computeIfAbsent(word.getValue(), k -> 0) + 1)
+        );
+        return freq;
     }
 
 }
